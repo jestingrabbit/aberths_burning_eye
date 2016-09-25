272f22c519bc8ee12d05c0e5a5a4cefa6e461737
Bill Cruickshank <jestingrabbit@gmail.com>
2016-09-25 22:44:30 +1000

    a little more node cruft

--- js/rootsOfUnity.js
+++ js/rootsOfUnity.js
@@ -9,14 +9,9 @@ class RootsOfUnity {
 
   get(n){
     if (!(this.roots[n])){
-      this.roots[n] = Array.from(new Array(5), (_, i) => {return i - Math.floor(n/2)} )
+      this.roots[n] = Array.from(new Array(n), (_, i) => {return i - Math.floor(n/2)} )
                         .map((i) => complex(1, i*twoPi/n, polar=true));
     }
     return this.roots[n];
   }
-}
-
-let rootsOfUnity = function(n){
-  return Array.from(new Array(5), (_, i) => i)
-              .map((i)=> complex(1, i*twoPi/n, polar=true));
 }
