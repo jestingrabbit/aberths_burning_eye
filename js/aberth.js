b2cf55cd229ca7f039896d88a04f92b7fa75dc5d
Bill Cruickshank <jestingrabbit@gmail.com>
2016-11-30 00:08:52 +1100

    prep for aberth method

--- js/aberth.js
+++ js/aberth.js
@@ -5,17 +5,22 @@ let roots = new RootsOfUnity();
 
 class aberth {
 
-  constructor(poly, soln){
+  constructor(poly, soln, stability = "low"){
     if (!soln) {
-      soln = roots[poly.degree()];
+      soln = roots.perturbed(poly.degree);
     }
     this.poly = poly;
     this.soln = soln;
+    this.stability = stability;
   }
 
-
-
-
-
-
+  step: (()=>{
+    if (stability == "low"){
+      return lowStabilityStep;
+    } else if (stability == "high") {
+      return highStabilityStep;
+    } else {
+      mehStabilityStep;
+    }
+  })()
 }
