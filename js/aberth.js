9769302207e4f115808621f806b31f7d3f0be84c
Bill Cruickshank <jestingrabbit@gmail.com>
2016-11-30 23:41:59 +1100

    adds newton correction and inverse to aberth

--- js/aberth.js
+++ js/aberth.js
@@ -1,9 +1,9 @@
-import { polynomial } from './polynomial.js';
+// import { polynomial } from './polynomial.js';
 import { RootsOfUnity } from './rootsOfUnity.js'
 
 let roots = new RootsOfUnity();
 
-class aberth {
+class Aberth {
 
   constructor(poly, soln, stability = "low"){
     if (!soln) {
@@ -12,15 +12,40 @@ class aberth {
     this.poly = poly;
     this.soln = soln;
     this.stability = stability;
+
+    if (this.stability == "low"){
+      this.step = Aberth.lowStabilityStep;
+    } else if (this.stability == "high") {
+      this.step = Aberth.highStabilityStep;
+    } else {
+      this.step = Aberth.mehStabilityStep;
+    }
   }
 
-  step: (()=>{
-    if (stability == "low"){
-      return lowStabilityStep;
-    } else if (stability == "high") {
-      return highStabilityStep;
-    } else {
-      mehStabilityStep;
-    }
-  })()
+  lowStabilityStep(){
+
+  }
+
+  newtonsCorrection(z){
+    return this.poly.evalAt(z)/this.poly.D().evalAt(z);
+  }
+
+  inverseNewtonsCorrection(z){
+    return this.poly.D().evalAt(z)/this.poly.evalAt(z);
+  }
+
+  sumsOfInversesOfDifferences(){
+    let matrix = Array.from({length: this.poly.degree},
+      (_, i)=>Array.from({length: this.poly.degree},
+        (__, j)=>[i, j]));
+    matrix.map(([i,j])=>)
+  }
+
+
+
+
+
+
 }
+
+export { Aberth }
