378ee0f576d1edc9831c97ac1e2a27ba89eec786
Bill Cruickshank <jestingrabbit@gmail.com>
2016-12-08 23:24:43 +1100

    better tests and more of them

--- js/complex.spec.js
+++ js/complex.spec.js
@@ -10,7 +10,30 @@ test("factory creates Complex", () => {
   expect(z.constructor.name).toBe("Complex");
 });
 
+test("responds to polar inputs", () => {
+  const z = complex(1, Math.PI/2, true);
+  expect(z.constructor.name).toBe("Complex");
+  expect(Math.log2(z.re)).toBeLessThan(-53); //precision of of 64 bit IEEE 754
+  expect(z.img).toEqual(1);
+});
+
+test("does complex addition", () => {
+  const z = (complex(1, 2)).add(3);
+  expect(z).toEqual(complex(4, 2));
+  const w = (complex(1, 2)).add(complex(3, 4));
+  expect(w).toEqual(complex(4, 6));
+});
+
+test("does complex subtraction", () => {
+  const z = (complex(1, 2)).sub(3);
+  expect(z).toEqual(complex(-2, 2));
+  const w = (complex(1, 2)).sub(complex(3, 4));
+  expect(w).toEqual(complex(-2, -2));
+});
+
 test("does complex multiplication", () => {
+  const w = (complex(1, 2)).mult(3);
+  expect(w).toEqual(complex(3, 6));
   const z = (complex(1, 2)).mult(complex(3, 4));
   expect(z).toEqual(complex(-5, 10));
 });
