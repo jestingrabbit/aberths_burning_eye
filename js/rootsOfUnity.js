460784f39f69b7cbf20d9ad230097f762e269910
Bill Cruickshank <jestingrabbit@gmail.com>
2016-12-11 00:33:57 +1100

    doc rootsOfUnity

--- js/rootsOfUnity.js
+++ js/rootsOfUnity.js
@@ -2,12 +2,27 @@ import { complex } from './complex.js';
 
 let twoPi = 2*Math.PI;
 
+
+/**
+ * @classdesc A bag for all the roots of unity, or those rotated by a random bit
+ * probably wont get much use in the usual run of things
+ */
 class RootsOfUnity {
 
-  construction(){
+  /**
+   * @constructor
+   */
+  constructor(){
     this.roots = {1: [complex(1)]};
   }
 
+
+  /**
+   * get - returns an array of the nth roots of 1
+   *
+   * @param  {number} n - which roots we want
+   * @return {Complex[]}   an array of the roots
+   */
   get(n){
     if (!(this.roots[n])){
       this.roots[n] = Array
@@ -18,6 +33,12 @@ class RootsOfUnity {
     return this.roots[n];
   }
 
+  /**
+   * perturbed - the nth roots randomly perturbed by the same amount
+   *
+   * @param  {type} n description
+   * @return {type}   description
+   */
   perturbed(n){
     const roots = this.get(n);
     const rand = twoPi*Math.random()/n;
